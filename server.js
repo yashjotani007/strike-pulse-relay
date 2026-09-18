4cce964dc678254a8647382894ce45ebf508f0a2
yashjotani007 <242675938+yashjotani007@users.noreply.github.com>
2026-09-18 09:54:39 +0530

    fix: load OI Analysis loader route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("./market-intelligence-api.js");
 require("./market-intelligence-loader-route.js");
 require("./oi-analysis-route.js");
+require("./oi-analysis-loader-route.js");
 require("./server-original.js");
