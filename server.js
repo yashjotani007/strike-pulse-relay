8ba89607af7b4b51f2556f63f454d1506ca6149a
yashjotani007 <242675938+yashjotani007@users.noreply.github.com>
2026-09-21 10:05:35 +0530

    feat: register Market Intelligence page data route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("./market-intelligence-api.js");
 require("./market-intelligence-loader-route.js");
+require("./market-intelligence-page-route.js");
 require("./oi-analysis-route.js");
 require("./oi-analysis-loader-route.js");
 require("./server-original.js");
